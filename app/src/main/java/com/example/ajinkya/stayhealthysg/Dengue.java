9b74405ad33ad1f2611c94c0d2dbeb0afb20c3a7
programmer1997 <ajinkyamarathe1997@gmail.com>
2017-03-25 01:49:46 +0800

    Cluster tab in dengue

--- app/src/main/java/com/example/ajinkya/stayhealthysg/Dengue.java
+++ app/src/main/java/com/example/ajinkya/stayhealthysg/Dengue.java
@@ -4,11 +4,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TabHost;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import static com.example.ajinkya.stayhealthysg.R.id.dengueMapActivity;
+
 /**
  * Created by Ajinkya on 24/3/17.
  */
 
-public class Dengue extends AppCompatActivity {
+public class Dengue extends AppCompatActivity implements OnMapReadyCallback{
+    MapFragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,29 +26,36 @@ public class Dengue extends AppCompatActivity {
         TabHost host=(TabHost) findViewById(R.id.dengueTabHost);
         host.setup();
 
-        //Tab 1
+        //Symptoms
         TabHost.TabSpec spec = host.newTabSpec("Symptoms");
         spec.setContent(R.id.tab1);
         spec.setIndicator("Symptoms");
         host.addTab(spec);
 
-        //Tab 2
+        //Prevention
         spec = host.newTabSpec("Prevention");
         spec.setContent(R.id.tab2);
         spec.setIndicator("Prevention");
         host.addTab(spec);
 
-        //Tab 3
+        //Clusters
         spec = host.newTabSpec("Clusters");
         spec.setContent(R.id.tab3);
         spec.setIndicator("Clusters");
         host.addTab(spec);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(dengueMapActivity);
+        mapFragment.getMapAsync(this);
+
+
+
 
     }
 
 
-
-
-
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(1.3521, 103.8198 ), 11.0f));
+    }
 }
