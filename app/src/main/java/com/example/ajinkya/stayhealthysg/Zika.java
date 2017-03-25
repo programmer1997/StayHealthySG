791a3b7f95c1bbe5eff4e16987c0f88e7850cd7f
rach Testing <rach0042@e.ntu.edu.sg>
2017-03-26 01:26:33 +0800

    Added Zika

---
+++ app/src/main/java/com/example/ajinkya/stayhealthysg/Zika.java
@@ -0,0 +1,63 @@
+package com.example.ajinkya.stayhealthysg;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import static com.example.ajinkya.stayhealthysg.R.id.zikaMapActivity;
+
+/**
+ * Created by rach on 25-Mar-17.
+ */
+
+public class Zika extends AppCompatActivity implements OnMapReadyCallback{
+    MapFragment fragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.malaria);
+        TabHost host=(TabHost) findViewById(R.id.zikaTabHost);
+        host.setup();
+
+        //Symptoms
+        TabHost.TabSpec spec = host.newTabSpec("Symptoms");
+        spec.setContent(R.id.zikaSymptoms);
+        spec.setIndicator("Symptoms");
+        host.addTab(spec);
+
+        //Prevention
+        spec = host.newTabSpec("Prevention");
+        spec.setContent(R.id.zikaPrevention);
+        spec.setIndicator("Prevention");
+        host.addTab(spec);
+
+        //Clusters
+        spec = host.newTabSpec("Clusters");
+        spec.setContent(R.id.zikaClusters);
+        spec.setIndicator("Clusters");
+        host.addTab(spec);
+
+        //SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        //      .findFragmentById(malariaMapActivity);
+        //mapFragment.getMapAsync(this);
+
+
+
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(1.3521, 103.8198 ), 11.0f));
+
+    }
+}
