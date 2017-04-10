31c11ac85a47fa638f5a94a9acf51892b023de57
andreveeguil <andreveeguil@yahoo.com>
2017-04-11 00:24:21 +0800

    Add search button to look for places in map

--- app/src/main/java/com/example/ajinkya/stayhealthysg/Diseases.java
+++ app/src/main/java/com/example/ajinkya/stayhealthysg/Diseases.java
@@ -8,6 +8,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -26,6 +28,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TabHost;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -44,6 +47,7 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Ajinkya on 11/3/17.
@@ -158,6 +162,25 @@ public class Diseases extends AppCompatActivity implements OnMapReadyCallback, G
     @Override
     public void onConnectionSuspended(int i) {
 
+    }
+
+    public void onMapSearch(View view){
+        EditText locationSearch = (EditText) findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+
+        if(location != null || location.equals("")){
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
     }
 
     @Override
