04844177c9c4bf24b4b6a7c67034c5d112b53783
BrigittaAngelina <brigittaxxangelina@gmail.com>
2017-04-11 02:42:03 +0800

    Deleting MapsActivity redundant code.

--- app/src/main/java/com/example/ajinkya/stayhealthysg/MapsActivity.java
+++ app/src/main/java/com/example/ajinkya/stayhealthysg/MapsActivity.java
@@ -33,7 +33,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.data.kml.KmlContainer;
 import com.google.maps.android.data.kml.KmlLayer;
+import com.google.maps.android.data.kml.KmlPlacemark;
+import com.google.maps.android.data.kml.KmlPolygon;
 
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -42,212 +45,64 @@ import java.util.Map;
 
 import static com.example.ajinkya.stayhealthysg.R.id.map_activity;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener{
-    double currentLatitude = 1.3521;
-    double currentLongitude = 103.8198;
-    private GoogleMap mGoogleMap;
-    private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
-    private Location mLastLocation;
-    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-    public static final String TAG = MapsActivity.class.getSimpleName();
-    final LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-    final String locationProvider = locationManager.GPS_PROVIDER;
-    final LocationListener locationListener = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            handleNewLocation(location);
-        }
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            if(status != LocationProvider.AVAILABLE) {
-                try {
-                    Location lastKnowLocation = locationManager.getLastKnownLocation(provider);
-                    currentLatitude = lastKnowLocation.getLatitude();
-                    currentLongitude = lastKnowLocation.getLongitude();
-                } catch (SecurityException e){
-                    e.printStackTrace();
-                }
-            }
-        }
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
-        public void onProviderEnabled(String provider) {
-            try {
-                Location location = locationManager.getLastKnownLocation(provider);
-                currentLatitude = location.getLatitude();
-                currentLongitude = location.getLongitude();
-            } catch(SecurityException e){
-                e.printStackTrace();
-            }
-        }
-
-        public void onProviderDisabled(String provider) {
-            try {
-                Location lastKnowLocation = locationManager.getLastKnownLocation(provider);
-                currentLatitude = lastKnowLocation.getLatitude();
-                currentLongitude = lastKnowLocation.getLongitude();
-            } catch(SecurityException e){
-                e.printStackTrace();
-            }
-        }
-    };
-
+    private GoogleMap map;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.diseases);
+        setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-
-        if(mGoogleApiClient==null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
-
-        // Create the LocationRequest object
-        mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
-                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.safestRoute);
+                .findFragmentById(map_activity);
         mapFragment.getMapAsync(this);
-        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(1.3521, 103.8198)).title("You are Here!"));
 
-        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+
+
+
+
+
+
+
+
     }
 
 
-    @Override
+
+
     public void onMapReady(GoogleMap googleMap) {
+
+        KmlLayer kmlLayer= null;
         try {
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(1.3521, 103.8198 ), 11.0f));
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                    mGoogleApiClient);
-            double currentLatitude = mLastLocation.getLatitude();
-            double currentLongitude = mLastLocation.getLongitude();
-
-            LatLng latLng = new LatLng(currentLatitude, currentLongitude);
-
-            //mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Current Location"));
-            MarkerOptions options = new MarkerOptions()
-                    .position(latLng)
-                    .title("You are here!");
-            googleMap.addMarker(options);
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        }
-        catch(SecurityException e){
-            e.printStackTrace();
-            Log.d(TAG,"Error");
-        }
-
-    }
-
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        try {
-
-            Log.i(TAG, "Location services connected.");
-            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            if (location == null) {
-                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, locationListener);
-            } else {
-                handleNewLocation(location);
+            kmlLayer = new KmlLayer(googleMap, R.raw.dengue,getApplicationContext());
+            kmlLayer.addLayerToMap();
+            KmlContainer container=kmlLayer.getContainers().iterator().next();
+            KmlPlacemark placemark=container.getPlacemarks().iterator().next();
+            KmlPolygon polygon=(KmlPolygon)placemark.getGeometry();
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            for (LatLng latLng : polygon.getOuterBoundaryCoordinates()) {
+                builder.include(latLng);
             }
 
-        }
-        catch(SecurityException e){
+            int width = getResources().getDisplayMetrics().widthPixels;
+            int height = getResources().getDisplayMetrics().heightPixels;
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), width, height, 1));
+
+        } catch (XmlPullParserException e) {
             e.printStackTrace();
-            Log.d(TAG, "Connection Error");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(1.3521, 103.8198 ), 11.0f));
+
+
+
+
+
+
+
+
     }
-
-
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.i(TAG, "Location services suspended. Please reconnect.");
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        /*
-         * Google Play services can resolve some errors it detects.
-         * If the error has a resolution, try sending an Intent to
-         * start a Google Play services activity that can resolve
-         * error.
-         */
-        if (connectionResult.hasResolution()) {
-            try {
-                // Start an Activity that tries to resolve the error
-                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
-                /*
-                 * Thrown if Google Play services canceled the original
-                 * PendingIntent
-                 */
-            } catch (IntentSender.SendIntentException e) {
-                // Log the error
-                e.printStackTrace();
-            }
-        } else {
-            /*
-             * If no resolution is available, display a dialog to the
-             * user with the error.
-             */
-            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
-        }
-    }
-
-    protected void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
-    }
-
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mGoogleApiClient.connect();
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (mGoogleApiClient.isConnected()) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, locationListener);
-            mGoogleApiClient.disconnect();
-        }
-    }
-
-
-
-
-
-    private void handleNewLocation(Location location) {
-        Log.d(TAG, location.toString());
-
-        double currentLatitude = location.getLatitude();
-        double currentLongitude = location.getLongitude();
-
-        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
-
-        //mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Current Location"));
-        MarkerOptions options = new MarkerOptions()
-                .position(latLng)
-                .title("You are here!");
-        mGoogleMap.addMarker(options);
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-    }
-
-
 }
-
